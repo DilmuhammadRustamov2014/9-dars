9210ec2ca863b88723f5a7acab15da51bb4e5069
DilmuhammadRustamov2014 <dilmuhammad.itf@gmail.com>
2025-01-19 22:30:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// const newTag = document.createElement("h1");
+// console.log(newTag);
+// newTag.textContent = "Dilshooooooood";
+// document.body.appendChild(newTag);
+
+
+
+// const newTag = document.createElement("img");
+// console.log(newTag);
+// newTag.textContent = "Dilshooooooood";
+// document.body.appendChild(newTag);
+
+
+// const img = document.createElement("img");
+// img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb-NGEQDekk2BwsllLjk4tcIM_BPIzXECdsg&s"
+// document.body.appendChild(img);
+
+
+// const Dilmuhammad = prompt("narsa yozing")
+// console.log(Dilmuhammad);
+// const newTag = document.createElement("h1")
+// newTag.textContent = "username"
+// document.body.append(Dilmuhammad)
+
+
+// const  = prompt("ismingizni kiriting");
+
+ 
+
+const Username = prompt("ismingizni kiriting");
+const Surname = prompt("familiyangizni kiriting");
+const Age = prompt("yoshingizni kiriting ");
+
+const object = {
+    ism:Username,
+    familiya: Surname,
+    yosh: Age,
+}
+
+
+const newTag = document.createElement("h1");
+    newTag.textContent = object.ism;
+    document.body.appendChild(newTag);
+
